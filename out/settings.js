e7ad2442b9f047eee37d39ea81387beef35391aa
Sopiro <movindeep@gmail.com>
2021-11-23 17:44:17 +0900

    Tweak parameter

--- out/settings.js
+++ out/settings.js
@@ -37,7 +37,7 @@ export const Settings = {
     },
     gravity: -10,
     penetrationSlop: 0.2,
-    restitutionSlop: 25,
+    restitutionSlop: 8,
     positionCorrectionBeta: 0.2,
     warmStartingThreshold: 0.08,
     deadBottom: -1000,
